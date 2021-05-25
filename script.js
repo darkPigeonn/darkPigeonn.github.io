2f0fb6ffe9ea31d5c4e9741c5382fd52cdebe433
AtanasiusIvannoel <50091059+darkPigeonn@users.noreply.github.com>
2021-05-25 08:49:45 +0700

    Initial commit
    new

---
+++ script.js
@@ -0,0 +1,131 @@
+(function(){
+  function build(){
+    const _0x4195=['103903oKuyMk','answers','1467029KvvjPy','2138AvzrQW','innerHTML','push','question','\x20:\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','155523SozqFO','<div\x20style=\x22background-color:#FFDA7B;\x22\x20class=\x22question\x22>\x20','363938rdiGnl','\x22\x20value=\x22','\x20</div>','\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22answers\x22>\x20','853WcAAPg','4YCWDdw','\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','1325034veGkbM','498022moJJOD','11bLYCsg','<label>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<input\x20type=\x22radio\x22\x20name=\x22question','join'];const _0x38b7f2=_0x1e1d;(function(_0x51b8ca,_0x2f9599){const _0x207c4a=_0x1e1d;while(!![]){try{const _0x24f552=-parseInt(_0x207c4a(0x201))+-parseInt(_0x207c4a(0x1fe))*-parseInt(_0x207c4a(0x1f9))+-parseInt(_0x207c4a(0x200))+-parseInt(_0x207c4a(0x1f7))+parseInt(_0x207c4a(0x1f2))*parseInt(_0x207c4a(0x1fd))+-parseInt(_0x207c4a(0x1f1))+parseInt(_0x207c4a(0x205))*parseInt(_0x207c4a(0x202));if(_0x24f552===_0x2f9599)break;else _0x51b8ca['push'](_0x51b8ca['shift']());}catch(_0x3d924e){_0x51b8ca['push'](_0x51b8ca['shift']());}}}(_0x4195,0xee797));function _0x1e1d(_0x4ba293,_0x5de67d){_0x4ba293=_0x4ba293-0x1f1;let _0x41952d=_0x4195[_0x4ba293];return _0x41952d;}const output=[];pertanyaan['forEach']((_0x1247cb,_0x57f494)=>{const _0x16fd7a=_0x1e1d,_0xc809e1=[];for(letter in _0x1247cb[_0x16fd7a(0x206)]){_0xc809e1[_0x16fd7a(0x1f4)](_0x16fd7a(0x203)+_0x57f494+_0x16fd7a(0x1fa)+letter+_0x16fd7a(0x1ff)+letter+_0x16fd7a(0x1f6)+_0x1247cb[_0x16fd7a(0x206)][letter]+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</label>');}output['push'](_0x16fd7a(0x1f8)+_0x1247cb[_0x16fd7a(0x1f5)]+_0x16fd7a(0x1fc)+_0xc809e1['join']('')+_0x16fd7a(0x1fb));}),quizContainer[_0x38b7f2(0x1f3)]=output[_0x38b7f2(0x204)]('');
+  }
+
+  function showResults(){
+
+    const _0x5d66=['.question','13529vdnZwy','647354QOcYCd','Sangat\x20Baik','35MGNEmx','value','862985nupnCp','597702KyxwVv','18789XCvGrr','2ACaebL','43XGxKGw','Baik','.answers','lightgreen','kurang','color','correctAnswer',']:checked','querySelector','input[name=question','1RdocvT','black','377699VsyIbZ','747614lPTvFr','style','<h3\x20style=\x22background-color:#FFDA7B;\x22>Nilai</h3><h3\x20id=\x22nilai\x22\x20style=\x22background-color:#96FA97;\x22>✔\x20','querySelectorAll'];const _0x2fdeb1=_0xa1cd;(function(_0x4350b7,_0x10666f){const _0x2f8cc0=_0xa1cd;while(!![]){try{const _0x540711=parseInt(_0x2f8cc0(0xe1))*-parseInt(_0x2f8cc0(0xd8))+-parseInt(_0x2f8cc0(0xd3))+parseInt(_0x2f8cc0(0xdb))*parseInt(_0x2f8cc0(0xdf))+-parseInt(_0x2f8cc0(0xdd))+parseInt(_0x2f8cc0(0xd0))*parseInt(_0x2f8cc0(0xd9))+-parseInt(_0x2f8cc0(0xd2))*-parseInt(_0x2f8cc0(0xe0))+parseInt(_0x2f8cc0(0xde));if(_0x540711===_0x10666f)break;else _0x4350b7['push'](_0x4350b7['shift']());}catch(_0x26dbe8){_0x4350b7['push'](_0x4350b7['shift']());}}}(_0x5d66,0x71b3b));const answerContainers=quizContainer[_0x2fdeb1(0xd6)](_0x2fdeb1(0xe3)),questionContainers=quizContainer[_0x2fdeb1(0xd6)](_0x2fdeb1(0xd7));let numCorrect=0x0;pertanyaan['forEach']((_0x56b6e2,_0x3f0a1f)=>{const _0x7fe9c2=_0x2fdeb1,_0x2bb10a=answerContainers[_0x3f0a1f],_0x28673b=_0x7fe9c2(0xcf)+_0x3f0a1f+_0x7fe9c2(0xcd),_0xce6d94=(_0x2bb10a[_0x7fe9c2(0xce)](_0x28673b)||{})[_0x7fe9c2(0xdc)];_0xce6d94===_0x56b6e2[_0x7fe9c2(0xcc)]?(numCorrect++,questionContainers[_0x3f0a1f][_0x7fe9c2(0xd4)][_0x7fe9c2(0xcb)]=_0x7fe9c2(0xd1),answerContainers[_0x3f0a1f][_0x7fe9c2(0xd4)]['color']=_0x7fe9c2(0xe4)):questionContainers[_0x3f0a1f][_0x7fe9c2(0xd4)][_0x7fe9c2(0xcb)]='red';});function _0xa1cd(_0x3c67f0,_0x59c12b){_0x3c67f0=_0x3c67f0-0xcb;let _0x5d667f=_0x5d66[_0x3c67f0];return _0x5d667f;}var kategori_nilai='';if(numCorrect>=0x1&&numCorrect<=0x4)var kategori_nilai=_0x2fdeb1(0xe5);else{if(numCorrect>=0x5&&numCorrect<=0x7)var kategori_nilai=_0x2fdeb1(0xe2);else{if(numCorrect>=0x8&&numCorrect<=0xa)var kategori_nilai=_0x2fdeb1(0xda);}}resultsContainer['innerHTML']=_0x2fdeb1(0xd5)+numCorrect*0xa+'\x20('+kategori_nilai+')</h3>',kategori_nilai='';
+	
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const pertanyaan = [
+    {
+      question: "<h2>1. Perhatikan gambar di samping!</h2><div class=\"tg-wrap\"><table style=\"undefined;table-layout: auto; width: 100%\"><tbody><tr><td><img src=\"https://i.postimg.cc/fRDjdmJQ/sdrhgrdshg.png\" style=\"width: 100%; height: auto;\"></td><td>Bagian yang ditunjukkan oleh nomor 3 dan 5 berfungsi sebagai...</td></tr></tbody></table></div>",
+      answers: {
+        a: "<p style=\"text-align: justify; text-justify: inter-word;\">Mengendalikan metabolisme dan mempercepat jalannya implus<p>",
+        b: "<p style=\"text-align: justify; text-justify: inter-word;\">Menerima implus dari sel lain untuk dikirim ke badan sel dan mengirimkan implus<p>",
+        c: "<p style=\"text-align: justify; text-justify: inter-word;\">Mempercepat jalannya implus dan menerima implus<p>",
+		d: "<p style=\"text-align: justify; text-justify: inter-word;\">Mengendalikan metabolisme dan mengirimkan implus<p>",
+		e: "<p style=\"text-align: justify; text-justify: inter-word;\">Mengirimkan implus dan menerima implus<p>"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "2. Neuron yang berfungsi untuk menghantarkan implus dari organ sensor ke saraf pusat yakni otak dan sumsum tulang belakang disebut...",
+      answers: {
+        a: "Eferen",
+        b: "Konektor",
+        c: "Aferen",
+		d: "Interneuron",
+		e: "Motor"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "3. Bagian sel saraf yang berfungsi sebagai alat komunikasi antar neuron disebut...",
+      answers: {
+        a: "Akson",
+        b: "Nodus ranvier",
+        c: "Mielyn",
+        d: "Sel schwan",
+		e: "Sinaps"
+      },
+      correctAnswer: "e"
+    },
+	{
+      question: "4. Pada saat saraf menghantarkan implus terdapat kondisi dimana saluran Na+ akan tebuka dan ion Na+ masuk ke dalam sel. Hal ini mengakibatkan terjadinya penurunan gradien listrik pada...",
+      answers: {
+        a: "<p style=\"text-align: justify; text-justify: inter-word;\">Bagian luar membran bermuatan (+) & permukaan dalam membran bermuatan (-)</p>",
+        b: "<p style=\"text-align: justify; text-justify: inter-word;\">Kedua bagian bermuatan (-)</p>",
+        c: "<p style=\"text-align: justify; text-justify: inter-word;\">Bagian dalam membran bermuatan positif (+) dan bagian luar membran pasif</p>",
+        d: "<p style=\"text-align: justify; text-justify: inter-word;\">Bagian luar membran bermuatan (-) dan bagian dalam membran bermuatan  (+)</p>",
+		e: "<p style=\"text-align: justify; text-justify: inter-word;\">Bagian dalam membran dan bagian luar membran pasif.</p>"
+      },
+      correctAnswer: "d"
+    },
+	{
+      question: "5. Berikut hal-hal yang berkaitan dengan penjalaran implus:<br><div class=\"tg-wrap\"><table style=\"undefined;table-layout: auto; width: 100%\"><tbody><tr><td>1. Efektor</td><td>3. Otak</td><td>5. Indra</td><td>7. Saraf Motor</td></tr><tr><td>2. Saraf sensori</td><td>4. Implus</td><td>6. Medula spinalis</td><td>8. Otot</td></tr></tbody></table></div><br>Mekanisme penghantar implus pada gerak refleks secara berurutan yang tepat adalah...",
+      answers: {
+        a: "5-1-2-4-7-6",
+        b: "4-5-2-6-7-1",
+        c: "5-2-4-6-7-8",
+        d: "4-5-2-5-8-1",
+		e: "2-5-7-4-3-5"
+      },
+      correctAnswer: "b"
+    },
+	{
+      question: "6. Sistem Saraf Pusat dibentuk oleh...",
+      answers: {
+        a: "Medulla spinallis dan medulla oblongata",
+        b: "Otak besar dan otak kecil",
+        c: "Otak dan medulla spinalis",
+        d: "Otak besar dan sistem limbik",
+		e: "Saraf simpatis dan saraf spinalis"
+      },
+      correctAnswer: "c"
+    },
+	{
+      question: "7. Pernyataan di bawah yang kurang tepat tentang bagian otak dan fungsinya adalah kecuali...",
+      answers: {
+        a: "<p style=\"text-align: justify; text-justify: inter-word;\">Otak kecil berfungsi untuk mengoordinasi pergerakan tubuh, mengatur proses belajar, dan mengingat respon motorik.</p>",
+        b: "<p style=\"text-align: justify; text-justify: inter-word;\">Talamus, sebagai pusat masukan untuk data sensorik yang akan menuju ke otak besar, pusat keluaran masuk respon motorik yang meninggalkan otak besar & menyaring data.</p>",
+        c: "<p style=\"text-align: justify; text-justify: inter-word;\">Hipotalamus, sebagai pusat kontrol homeostatis, mengontrol kelenjar pituitari dan berperan dalam jam biologis.</p>",
+        d: "<p style=\"text-align: justify; text-justify: inter-word;\">Medula oblongata, sebagai pengontrol pernapasan, sirkulasi, penelanan dan pencernaan.</p>",
+		e: "<p style=\"text-align: justify; text-justify: inter-word;\">Otak tengah, sebagai pusat kontrol memori, belajar, berbicara dan emosi. </p>"
+      },
+      correctAnswer: "e"
+    },
+	{
+      question: "8. Lapisan terluar pelindung otak yang bersifat tebal dan sangat kuat disebut...",
+      answers: {
+        a: "Pia meter",
+        b: "Dura meter",
+        c: "Araknoid",
+        d: "Meninges",
+		e: "Korteks"
+      },
+      correctAnswer: "b"
+    },
+	{
+      question: "9. Perhatikan gambar disamping!<div class=\"tg-wrap\"><table style=\"undefined;table-layout: auto; width: 100%\"><tbody><tr><td><br><p style=\"text-align: justify; text-justify: inter-word;\">Apabila seseorang mengalami kecelakaan kemudian mengalami gangguan pada penglihatan, maka kemungkinan otak yang mengalami kerusakan terdapat pada bagian yang ditunjukkan oleh nomor...</p></td><td><img src=\"https://i.postimg.cc/GmFSDvrk/gergerg.png\" style=\"width: 100%; height: auto;\"></td></tr></tbody></table></div>",
+      answers: {
+        a: "4",
+        b: "1",
+        c: "3",
+        d: "5",
+		e: "2"
+      },
+      correctAnswer: "a"
+    },
+	{
+      question: "10. Pada gambar di samping, <div class=\"tg-wrap\"><table style=\"undefined;table-layout: auto; width: 100%\"><tbody><tr><td>bagian yang ditunjukkan oleh huruf X merupakan...</td><td><img src=\"https://i.postimg.cc/7YsSW6Vp/erherthrt.png\" style=\"width: 100%; height: auto;\"></td></tr></tbody></table></div>",
+      answers: {
+        a: "Batang Otak, berfungsi untuk mengkoordinir pergerakan tubuh",
+        b: "Otak tengah, berfungsi mengkoordinasi refleks",
+        c: "Serebelum, berfungsi sebagai pusat penglihatan",
+        d: "Talamus, berfungsi untuk pemasukan data sensorik ke dalam otak besar",
+		e: "Pons, berfungsi untuk mengatur pernapasan"
+      },
+      correctAnswer: "c"
+    }
+  ];
+
+  build();
+
+  submitButton.addEventListener('click', showResults);
+})();
